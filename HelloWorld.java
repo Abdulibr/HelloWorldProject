42721b9d89b242d4278a4b286bae4fa0964d5bc4
Birkaan_20@hotmail.co.uk <Birkaan_20@hotmail.co.uk>
2019-01-31 18:58:13 +0000

    First project commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
-class HelloClass
+public class HelloWorld
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-	System.out.println("Hello World");
-	
+	System.out.println("Hello World!");
     }
-		
+
 }
